cd893a59de66207c7f884f69d5cfd43a7abbea0c
zain <zsyed44@uwo.ca>
2025-01-24 14:35:21 -0500

    Adding a setting up necessary files

---
+++ mpcTools/vehicle_model.py
@@ -0,0 +1,27 @@
+# The following code was imported from the following repository:
+# https://github.com/mcarfagno/mpc_python/tree/master?tab=readme-ov-file
+# This file in specific has not been modified, but has been commented for readability
+
+import numpy as np
+
+
+class VehicleModel:
+    """
+    Helper class that contains the parameters of the vehicle to be controlled
+
+    Attributes:
+        wheelbase: [m] 
+        max_speed: [m/s]
+        max_acc: [m/ss]
+        max_d_acc: [m/sss]
+        max_steer: [rad]
+        max_d_steer: [rad/s]
+    """
+
+    def __init__(self):
+        self.wheelbase = 0.3 # Distance betwee
+        self.max_speed = 1.5
+        self.max_acc = 1.0
+        self.max_d_acc = 1.0
+        self.max_steer = np.radians(30)
+        self.max_d_steer = np.radians(30)
